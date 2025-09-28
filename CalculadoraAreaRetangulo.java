c80dda9e2e4c6bd6b6fee0e56126ac2ce065424f
“victorsousalemose” <victorsousalemos@hotmail.coml>
2025-09-28 20:32:15 -0300

    Primeiro commit do curso de Java

---
+++ CalculadoraAreaRetangulo.java
@@ -0,0 +1,12 @@
+public class CalculadoraAreaRetangulo {
+	public static void main(String[] args) {
+		double largura = 0.5D;
+		double altura = 3.0D;
+		double area = largura * altura;
+
+
+		System.out.println("Largura do retangulo: " + largura);
+		System.out.println("Altura do retangulo: " + altura);
+		System.out.println("Área do retângulo:" + area);
+	}
+}
---
+++ PromocaoAutomatica.java
@@ -0,0 +1,28 @@
+public class PromocaoAutomatica{
+	public static void main(String[] args) {
+		byte a = 10;
+		short b = 20;
+		char c = 'A';
+		int d = 30;
+		long e = 40L;
+		float f = 1.5f;
+		double g = 2.5d;
+
+		// Exemplo de promoção automática: byte e short promovidos para int
+		int resultado =	a + b; System.out.println("Resultado da soma: "	+ resultado);
+
+		// Mais exemplos com outros tipos
+		long resultadoLong = d + e;
+		// int promovido para long
+		float resultadoFloat = d * f;
+		// int promovido para float
+		double resultadoDouble = d * g;
+		// int promovido para double
+		System.out.println("Resultado long: " + resultadoLong);
+		System.out.println("Resultado float: " + resultadoFloat);
+		System.out.println("Resultado double: " + resultadoDouble);
+		// Exibindo o valor do char (conteúdo)
+		System.out.println("Valor do char: " + c);
+
+	}
+}
