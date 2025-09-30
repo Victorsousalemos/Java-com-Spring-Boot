53f30533678bfa6260d12235c8ce7aa1827572c2
“victorsousalemose” <victorsousalemos@hotmail.coml>
2025-09-29 21:24:53 -0300

    aprendendo Operadores

---
+++ IncrementoDecremento.java
@@ -0,0 +1,14 @@
+public class IncrementoDecremento {
+	public static void main(String[] args) {
+		
+		int contador = 5;
+
+		// incremento
+		contador++;     // Equivalente a contador = contador + 1;
+
+		// decremento
+		contador--;    // Equivalente a contador = contador - 1;
+
+		System.out.println("Valor final do contador: " + contador);
+	}
+}
